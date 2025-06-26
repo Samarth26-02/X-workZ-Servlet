718901eb6633f40d3074d7e2613611870c3fea5d
T S Samarth <tssam1547@gmail.com>
2025-06-27 00:10:16 +0530

    ContactServlet.java

--- src/com/xworkz/contact/servlet/ContactServlet.java
+++ src/com/xworkz/contact/servlet/ContactServlet.java
@@ -5,7 +5,6 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
-// calls servlet
 @WebServlet(urlPatterns = "/contact",loadOnStartup = 1)
 
 public class ContactServlet extends GenericServlet {
